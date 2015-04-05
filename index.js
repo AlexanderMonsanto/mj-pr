e85ff2c13c9b2b0f7789164106af0bd43faf5fd6
alexandermonsanto <alexandermonsanto@gmail.com>
2015-04-05 15:55:57 -0700

    site sans email

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(__dirname + "/public"))
+
+app.set("view engine", 'ejs');
+
+app.get('/',function(req, res){
+  res.render('index')
+})
+
+
+app.listen(process.env.PORT || 3000);
